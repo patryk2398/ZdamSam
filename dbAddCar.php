359906916cd06b1078a7e65365d53523887b7140
MacSadecki <95184828+MacSadecki@users.noreply.github.com>
2021-12-04 18:53:14 +0100

    Added option to add cars

---
+++ dbAddCar.php
@@ -0,0 +1,15 @@
+<?php
+
+$con=mysqli_connect("localhost","root","","zdamsam");
+
+$user_info = "INSERT INTO cars (brand, model, productionYear, regNumber, mileage, serviceDate, ocDate, acDate) VALUES ('$_POST[brand]', '$_POST[model]','$_POST[productionYear]','$_POST[regNumber]','$_POST[mileage]','$_POST[serviceDate]','$_POST[ocDate]','$_POST[acDate]')"; 
+
+mysqli_query($con, $user_info);
+echo "Dodano pojazd do bazy";
+
+mysqli_close($con);  
+?>
+<html>
+<p>Za chwilę zostaniesz przeniesiony</p>
+<meta http-equiv="refresh" content="0;url=car_form.html" />
+</html>
