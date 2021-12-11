78a6e82d780f61ca2a3154f581f19e9bf6ce47b2
KamilCz <94784108+KamilCzerewko@users.noreply.github.com>
2021-12-11 11:15:49 +0100

    Fixed sessions login form

--- dbLogin.php
+++ dbLogin.php
@@ -1,27 +1,27 @@
 <?php
 header( "refresh:0;url=login.php" );
 $user = $_POST['login'];
-$pass = md5($_POST['pass']);
+$pass = $_POST['pass'];
 
 if ($user&&$pass) 
 	{
 	//connect to db
 	$connect = mysqli_connect("localhost","root","","zdamsam");
-	$query = mysqli_query($connect, "SELECT * FROM login WHERE email='$user'");
+	$query = mysqli_query($connect, "SELECT * FROM login WHERE email='$user' AND password='$pass'");
 
 	$numrows = mysqli_num_rows($query);
-
 
 	if ($numrows!=0)
 	{
 	//while loop
-	  while ($row = mysqli_fetch_assoc($query))
-	  {
-		$dbusername = $row['email'];
-		$dbpassword = $row['password'];
-		$_SESSION['login'] = $row['email'];
-		header("location: car_list.php");
-	  }
+	while ($row = mysqli_fetch_assoc($query))
+	{
+	$dbusername = $row['email'];
+	$dbpassword = $row['password'];
+	session_start();
+	$_SESSION['login'] = 1;
+	header("location: car_list.php");
+	}
 	  
 	}
 	else
